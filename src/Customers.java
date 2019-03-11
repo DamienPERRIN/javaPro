b70776fe5d00696f89e565682bbd811ab77790dc
DamienPERRIN <daperr87@gmail.com>
2019-03-11 14:30:28 +0100

    Create one table with bootstrap 4 in index.html and declare getters and setters in Customers.java

---
+++ src/Customers.java
@@ -0,0 +1,86 @@
+
+public class Customers
+{
+    private int id;
+    private String name, password, email, country;
+
+    /**
+     *
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     *
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     *
+     * @param country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
